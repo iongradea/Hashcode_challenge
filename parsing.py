a56d390728f07e5496a638267694eb18a8eb1273
Eskombro <sjimenezre@gmail.com>
2019-03-04 15:01:02 +0100

    First parsing

--- parsing.py
+++ parsing.py
@@ -20,7 +20,39 @@ def create_v_2_x():
 		obj = {'id1': v_list[i]['id1'], 'id2': v_list[i + 1]['id1'], 'fmt': 'X', 'nb': len(tab), 'tags': tab}
 		x_list.append(obj)
 		i += 2
-	print(x_list)
+	##print(x_list)
+
+def calc_i(a, b):
+	tot = a['tags'].copy()
+	tot.extend(b['tags'])
+	tot = list(set(tot))
+	lst = []
+	lst = list(set(a['tags']).intersection(b['tags']))
+	x_i = len(lst)
+	b_i = len(list(set(tot) - set(a['tags'])))
+	a_i = len(list(set(tot) - set(b['tags'])))
+	return (min(x_i, a_i, b_i))
+
+def calc_slide(end_list):
+	slides = []
+	while (len(end_list) > 0):
+		i = 0
+		fact_i_max = 0
+		i_max = 1
+		while (i < len(end_list)):
+			if (calc_i(end_list[0], end_list[i]) > fact_i_max):
+				fact_i_max = calc_i(end_list[0], end_list[i])
+				i_max = i
+			i += 1
+		if (len(end_list) == 1):
+			first = end_list.pop(0)
+			slides.append(first)
+		else:
+			second = end_list.pop(i_max)
+			first = end_list.pop(0)
+			slides.append(first)
+			slides.append(second)
+	return (slides)
 
 fd = open(sys.argv[1], 'r')
 img_ct = int(fd.readline())
@@ -37,17 +69,13 @@ while(rl):
 create_v_2_x()
 end_list = h_list.copy()
 end_list.extend(x_list)
-print(end_list)
 end_list = sorted(end_list, key=lambda k: int(k['nb']), reverse=True)
-print(end_list)
+slides = calc_slide(end_list)
 
-def create_vertical_slide():
-	pass
-
-def calc_i(slide_a, slide_b):
-	pass
-
-
-
-def calc_score(slide_list):
-	pass
+print(len(slides))
+for i in slides:
+	line = str(i['id1'])
+	if (i['id2']):
+		line += " "
+		line += str(i['id1'])
+	print(line)
